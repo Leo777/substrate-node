1a2fb8809edd7e2f74416f31fee301abff113091
Lev777 <borichevskiy@gmail.com>
2022-07-03 22:17:47 +0200

    fixed imports

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,19 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::sp_runtime::{
+	offchain::{
+		storage::{MutateStorageError, StorageRetrievalError, StorageValueRef}
+	}
+};
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
-use sp_runtime::{
-	offchain::{
-		http,
-		storage::{MutateStorageError, StorageRetrievalError, StorageValueRef},
-		Duration,
-	},
-	traits::Zero,
-	transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction},
-	RuntimeDebug,
-};
 
 #[cfg(test)]
 mod mock;
@@ -26,6 +21,7 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -35,6 +31,9 @@ pub mod pallet {
 		type Event: From<Event<Self>>
 			+ IsType<<Self as frame_system::Config>::Event>
 			+ TryInto<Event<Self>>;
+
+		#[pallet::constant]
+		type GracePeriod: Get<Self::BlockNumber>;
 	}
 
 	#[pallet::pallet]
@@ -60,7 +59,8 @@ pub mod pallet {
 	}
 
 	enum TransactionType {
-		Signed
+		Signed,
+		None,
 	}
 
 	// Errors inform users that something went wrong.
@@ -113,67 +113,36 @@ pub mod pallet {
 				},
 			}
 		}
-	
 	}
 
 	impl<T: Config> Pallet<T> {
 		fn choose_transaction_type(block_number: T::BlockNumber) -> TransactionType {
-			/// A friendlier name for the error that is going to be returned in case we are in the grace
-			/// period.
 			const RECENTLY_SENT: () = ();
-	
-			// Start off by creating a reference to Local Storage value.
-			// Since the local storage is common for all offchain workers, it's a good practice
-			// to prepend your entry with the module name.
+
 			let val = StorageValueRef::persistent(b"example_ocw::last_send");
-			// The Local Storage is persisted and shared between runs of the offchain workers,
-			// and offchain workers may run concurrently. We can use the `mutate` function, to
-			// write a storage entry in an atomic fashion. Under the hood it uses `compare_and_set`
-			// low-level method of local storage API, which means that only one worker
-			// will be able to "acquire a lock" and send a transaction if multiple workers
-			// happen to be executed concurrently.
-			let res = val.mutate(|last_send: Result<Option<T::BlockNumber>, StorageRetrievalError>| {
-				match last_send {
-					// If we already have a value in storage and the block number is recent enough
-					// we avoid sending another transaction at this time.
-					Ok(Some(block)) if block_number < block + T::GracePeriod::get() =>
-						Err(RECENTLY_SENT),
-					// In every other case we attempt to acquire the lock and send a transaction.
-					_ => Ok(block_number),
-				}
-			});
-	
-			// The result of `mutate` call will give us a nested `Result` type.
-			// The first one matches the return of the closure passed to `mutate`, i.e.
-			// if we return `Err` from the closure, we get an `Err` here.
-			// In case we return `Ok`, here we will have another (inner) `Result` that indicates
-			// if the value has been set to the storage correctly - i.e. if it wasn't
-			// written to in the meantime.
+
+			let res =
+				val.mutate(|last_send: Result<Option<T::BlockNumber>, StorageRetrievalError>| {
+					match last_send {
+						Ok(Some(block)) if block_number < block + T::GracePeriod::get() =>
+							Err(RECENTLY_SENT),
+						_ => Ok(block_number),
+					}
+				});
+
 			match res {
-				// The value has been set correctly, which means we can safely send a transaction now.
-				Ok(block_number) => {
-					// Depending if the block is even or odd we will send a `Signed` or `Unsigned`
-					// transaction.
-					// Note that this logic doesn't really guarantee that the transactions will be sent
-					// in an alternating fashion (i.e. fairly distributed). Depending on the execution
-					// order and lock acquisition, we may end up for instance sending two `Signed`
-					// transactions in a row. If a strict order is desired, it's better to use
-					// the storage entry for that. (for instance store both block number and a flag
-					// indicating the type of next transaction to send).
-					
-						TransactionType::Signed
-					
-				},
-				// We are in the grace period, we should not send a transaction this time.
-				Err(MutateStorageError::ValueFunctionFailed(RECENTLY_SENT)) => TransactionType::None,
-				// We wanted to send a transaction, but failed to write the block number (acquire a
-				// lock). This indicates that another offchain worker that was running concurrently
-				// most likely executed the same logic and succeeded at writing to storage.
-				// Thus we don't really want to send the transaction, knowing that the other run
-				// already did.
+				Ok(block_number) => TransactionType::Signed,
+				Err(MutateStorageError::ValueFunctionFailed(RECENTLY_SENT)) =>
+					TransactionType::None,
+				
 				Err(MutateStorageError::ConcurrentModification(_)) => TransactionType::None,
 			}
-		}	
+		}
+		fn call_api_and_send_transaction() -> Result<(), &'static str> {
+
+			//TODO
+			Ok(())
+		}
 	}
 
 	#[pallet::hooks]
@@ -189,14 +158,14 @@ pub mod pallet {
 				.any(|event| matches!(event, Event::SomethingStored(..)))
 			{
 				let should_send = Self::choose_transaction_type(block_number);
-			let res = match should_send {
-				TransactionType::Signed => Self::fetch_price_and_send_signed(),
-				
-			};
-			if let Err(e) = res {
-				log::error!("Error: {}", e);
-			}
-			log::info!("Hello world from OCW {:?}", block_number);
+				let res = match should_send {
+					TransactionType::Signed => Self::call_api_and_send_transaction(),
+					TransactionType::None => todo!()
+				};
+				if let Err(e) = res {
+					log::error!("Error: {}", e);
+				}
+				log::info!("Hello world from OCW {:?}", block_number);
 			}
 		}
 	}
