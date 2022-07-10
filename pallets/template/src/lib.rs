6fadd2647226ba95bae9e81c2150b8928aa78645
Barycheuski, Leu <Leu.Barycheuski@sabre.com>
2022-07-10 12:39:54 +0200

    Http call

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,12 +1,33 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-
+use core::{convert::TryInto, fmt};
 use frame_support::sp_runtime::offchain::storage::{
 	MutateStorageError, StorageRetrievalError, StorageValueRef,
 };
+use serde::{Deserialize, Deserializer};
+
+use parity_scale_codec::{Decode, Encode};
+
+use frame_support::sp_runtime::{
+	offchain as rt_offchain,
+	offchain::storage_lock::{BlockAndTime, StorageLock},
+	transaction_validity::{
+		InvalidTransaction, TransactionSource, TransactionValidity, ValidTransaction,
+	},
+	RuntimeDebug,
+};
+
+use frame_support::sp_std::{collections::vec_deque::VecDeque, prelude::*, str};
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
+
+const HTTP_REMOTE_REQUEST: &str = "https://api.github.com/orgs/substrate-developer-hub";
+const HTTP_HEADER_USER_AGENT: &str = "jimmychu0807";
+
+const FETCH_TIMEOUT_PERIOD: u64 = 3000; // in milli-seconds
+const LOCK_TIMEOUT_EXPIRATION: u64 = FETCH_TIMEOUT_PERIOD + 1000; // in milli-seconds
+const LOCK_BLOCK_EXPIRATION: u32 = 3; // in block number
 
 #[cfg(test)]
 mod mock;
@@ -17,12 +38,48 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
+// ref: https://serde.rs/container-attrs.html#crate
+#[derive(Deserialize, Encode, Decode, Default)]
+struct GithubInfo {
+	// Specify our own deserializing function to convert JSON string to vector of bytes
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	login: Vec<u8>,
+	#[serde(deserialize_with = "de_string_to_bytes")]
+	blog: Vec<u8>,
+	public_repos: u32,
+}
+
+#[derive(Debug, Deserialize, Encode, Decode, Default)]
+struct IndexingData(Vec<u8>, u64);
+
+pub fn de_string_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	let s: &str = Deserialize::deserialize(de)?;
+	Ok(s.as_bytes().to_vec())
+}
+
+impl fmt::Debug for GithubInfo {
+	// `fmt` converts the vector of bytes inside the struct back to string for
+	//   more friendly display.
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(
+			f,
+			"{{ login: {}, blog: {}, public_repos: {} }}",
+			str::from_utf8(&self.login).map_err(|_| fmt::Error)?,
+			str::from_utf8(&self.blog).map_err(|_| fmt::Error)?,
+			&self.public_repos
+		)
+	}
+}
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	
+
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -69,6 +126,7 @@ pub mod pallet {
 		NoneValue,
 		/// Errors should have helpful documentation associated with them.
 		StorageOverflow,
+		HttpFetchingError,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -143,14 +201,64 @@ pub mod pallet {
 
 		fn call_api_and_send_transaction() -> Result<(), &'static str> {
 			//TODO
-			let validator = sp_io::offchain::is_validator();
+			let result = Self::fetch_n_parse()?;
 
-			if validator {
-				log::info!("Calling api and sending transaction ");
-				log::info!("is validator {:?} ", validator);
-			}
-			
+			log::info!("Calling api and sending transaction ");
+			log::info!("is Result {:?} ", result);
+
 			Ok(())
+		}
+
+		/// Fetch from remote and deserialize the JSON to a struct
+		fn fetch_n_parse() -> Result<GithubInfo, Error<T>> {
+			let resp_bytes = Self::fetch_from_remote().map_err(|e| {
+				log::error!("fetch_from_remote error: {:?}", e);
+				<Error<T>>::HttpFetchingError
+			})?;
+
+			let resp_str =
+				str::from_utf8(&resp_bytes).map_err(|_| <Error<T>>::HttpFetchingError)?;
+			// Print out our fetched JSON string
+			log::info!("{}", resp_str);
+
+			// Deserializing JSON to struct, thanks to `serde` and `serde_derive`
+			let gh_info: GithubInfo =
+				serde_json::from_str(resp_str).map_err(|_| <Error<T>>::HttpFetchingError)?;
+			Ok(gh_info)
+		}
+
+		fn fetch_from_remote() -> Result<Vec<u8>, Error<T>> {
+			// Initiate an external HTTP GET request. This is using high-level wrappers from `sp_runtime`.
+			let request = rt_offchain::http::Request::get(HTTP_REMOTE_REQUEST);
+
+			// Keeping the offchain worker execution time reasonable, so limiting the call to be within 3s.
+			let timeout = sp_io::offchain::timestamp()
+				.add(rt_offchain::Duration::from_millis(FETCH_TIMEOUT_PERIOD));
+
+			// For github API request, we also need to specify `user-agent` in http request header.
+			//   See: https://developer.github.com/v3/#user-agent-required
+			let pending = request
+				.add_header("User-Agent", HTTP_HEADER_USER_AGENT)
+				.deadline(timeout) // Setting the timeout time
+				.send() // Sending the request out by the host
+				.map_err(|_| <Error<T>>::HttpFetchingError)?;
+
+			// By default, the http request is async from the runtime perspective. So we are asking the
+			//   runtime to wait here.
+			// The returning value here is a `Result` of `Result`, so we are unwrapping it twice by two `?`
+			//   ref: https://substrate.dev/rustdocs/v3.0.0/sp_runtime/offchain/http/struct.PendingRequest.html#method.try_wait
+			let response = pending
+				.try_wait(timeout)
+				.map_err(|_| <Error<T>>::HttpFetchingError)?
+				.map_err(|_| <Error<T>>::HttpFetchingError)?;
+
+			if response.code != 200 {
+				log::error!("Unexpected http request status code: {}", response.code);
+				return Err(<Error<T>>::HttpFetchingError);
+			}
+
+			// Next we fully read the response body and collect it to a vector of bytes.
+			Ok(response.body().collect::<Vec<u8>>())
 		}
 	}
 
